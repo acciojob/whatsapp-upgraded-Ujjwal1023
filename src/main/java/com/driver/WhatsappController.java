ab55f4bca5874944626961f22a4daae0707897cd
UJJWAL'S_WORLD <kumarujjwal471@gmail.com>
2023-07-22 23:34:23 +0530

    whatsapp

--- src/main/java/com/driver/WhatsappController.java
+++ src/main/java/com/driver/WhatsappController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("whatsapp")
-public class WhatsappController {
+public class  WhatsappController {
 
     //Autowire will not work in this case, no need to change this and add autowire
     WhatsappService whatsappService = new WhatsappService();
